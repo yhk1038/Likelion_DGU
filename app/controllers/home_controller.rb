14b1c1cf40b92ef9ce5430b13a64d058362f6856
yhk1038 <yhk1038@hanmail.net>
2015-11-24 15:45:40 +0000

    first set

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def intro
+  end
+
+  def index
+  end
+
+  def main
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root :to => "home#intro"
+  
+  match ":controller(/:action(/:id))", :via => [:get, :post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
